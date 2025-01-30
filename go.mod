6114ef3f3f2098e3391901f60084b74c11eed67d
KaidenTheCat <kaidencat270@gmail.com>
2025-01-30 12:00:55 +0200

    Init Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module prj_test
+
+go 1.23.5
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"time"
+)
+
+func main() {
+	fmt.Printf("Оцінка Дениса по життю %g.\n", math.Sqrt(7))
+	time.Sleep(5 * time.Second)
+
+}
