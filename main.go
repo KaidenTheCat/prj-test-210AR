c297560d43f23670c748740bb1d87d2cd7ebbca0
KaidenTheCat <kaidencat270@gmail.com>
2025-02-17 08:49:16 +0200

    Add: GameProto

--- main.go
+++ main.go
@@ -2,12 +2,48 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"math/rand"
+	"strconv"
 	"time"
 )
 
+const (
+	totalPoints       = 100
+	pointsPerQuestion = 20
+)
+
 func main() {
-	fmt.Printf("Оцінка Дениса по життю %g.\n", math.Sqrt(7))
-	time.Sleep(5 * time.Second)
+	fmt.Println("Вітаємо у грі ---!")
+
+	for i := 5.0; i > 0; i -= 0.1 {
+		fmt.Printf("\rДо початку: %v", i)
+		time.Sleep(100)
+		if i == 1 {
+			fmt.Print("\rГра почалась!")
+		}
+	}
+
+	myPoints := 0
+
+	for myPoints < totalPoints {
+		x, y := rand.Intn(100), rand.Intn(100)
+		fmt.Printf("\n%v + %v = ", x, y)
+
+		ans := ""
+		fmt.Scan(&ans)
+
+		ansInt, err := strconv.Atoi(ans)
+
+		if err != nil {
+			fmt.Println("Невдале знаяення, давай по новой")
+		} else {
+			if ansInt == x+y {
+				myPoints += pointsPerQuestion
+				fmt.Printf("Правильно! У вас %v очок!", myPoints)
+			} else {
+				fmt.Println("Не праивльно!")
+			}
+		}
+	}
 
 }
