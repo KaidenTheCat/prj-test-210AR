d125443a69e41fcf623b7e287b9c8e3a288b1261
KaidenTheCat <kaidencat270@gmail.com>
2025-02-27 10:21:35 +0200

    v 1.0

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"log"
 	"math/rand"
+	"os"
+	"prj_test/domain"
+	"sort"
 	"strconv"
 	"time"
 )
@@ -12,21 +18,59 @@ const (
 	pointsPerQuestion = 20
 )
 
-func main() {
-	fmt.Println("Вітаємо у грі ---!")
+var id uint64 = 1
 
-	for i := 5.0; i > 0; i -= 0.1 {
-		fmt.Printf("\rДо початку: %v", i)
-		time.Sleep(100)
-		if i == 1 {
-			fmt.Print("\rГра почалась!")
+func main() {
+	fmt.Println("Вітаємо у грі!")
+
+	users := getUsers()
+	for _, user := range users {
+		if user.Id >= id {
+			id = user.Id + 1
 		}
 	}
 
+	sortAndSave(users)
+	for {
+		menu()
+
+		choise := ""
+		fmt.Scan(&choise)
+
+		switch choise {
+		case "1":
+			user := play()
+			users = getUsers()
+			users = append(users, user)
+			sortAndSave(users)
+		case "2":
+			users = getUsers()
+			for _, u := range users {
+				fmt.Printf(
+					"Id: %v, Name: %s, Time: %v\n",
+					u.Id, u.Name, u.TimeSpent,
+				)
+			}
+		case "3":
+			return
+		default:
+		}
+	}
+
+}
+
+func menu() {
+	fmt.Println("1. Грати")
+	fmt.Println("2. Рейтинг")
+	fmt.Println("3. Вийти")
+}
+
+func play() domain.User {
+	TimeStart := time.Now()
 	myPoints := 0
 
 	for myPoints < totalPoints {
-		x, y := rand.Intn(100), rand.Intn(100)
+		x, y := rand.Intn(2), rand.Intn(2)
 		fmt.Printf("\n%v + %v = ", x, y)
 
 		ans := ""
@@ -44,6 +88,75 @@ func main() {
 				fmt.Println("Не праивльно!")
 			}
 		}
+
 	}
 
+	TimeFinish := time.Now()
+	timeSpent := TimeFinish.Sub(TimeStart)
+
+	fmt.Printf("Ваш час: %v", timeSpent)
+	fmt.Print("Введіть ваше ім'я: ")
+
+	name := ""
+	fmt.Scan(&name)
+
+	user := domain.User{
+		Id:        id,
+		Name:      name,
+		TimeSpent: timeSpent,
+	}
+	id++
+
+	return user
+}
+
+func sortAndSave(users []domain.User) {
+	sort.SliceStable(users, func(i int, j int) bool {
+		return users[i].TimeSpent < users[j].TimeSpent
+	})
+
+	file, err := os.OpenFile("users.json", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
+	if err != nil {
+		log.Printf("sortAndSave(os.OpenFile): %s", err)
+		return
+	}
+
+	defer func() {
+		err = file.Close()
+		if err != nil {
+			log.Printf("sortAndSave(file.Close()): %s", err)
+		}
+	}()
+
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(users)
+	if err != nil {
+		log.Printf("sortAndSave(encoder.Encode)): %s", err)
+		return
+	}
+}
+
+func getUsers() []domain.User {
+	var users []domain.User
+	file, err := os.Open("users.json")
+
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			_, err = os.Create("users.jsom")
+			if err != nil {
+				log.Printf("getUsers(os.Create): %s", err)
+			}
+			return nil
+		}
+		log.Printf("getUsers(os.Open): %s", err)
+		return nil
+	}
+
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&users)
+	if err != nil {
+		log.Printf("getUsers(decoder.Decode): %s", err)
+	}
+
+	return users
 }
